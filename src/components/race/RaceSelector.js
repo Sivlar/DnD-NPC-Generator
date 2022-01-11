fd4fde1dae014ca96cc713401beab2327497069d
goodnamesarerare <xenga7@gmail.com>
2022-01-11 01:05:41 -0500

    Update Race Selector with Random
    Add a race randomizer button

--- src/components/race/RaceSelector.js
+++ src/components/race/RaceSelector.js
@@ -1,15 +1,18 @@
 import races from "../../data/races.json"
+import { useState } from 'react';
 
 function RaceSelector() {
+  const [randNum, setrandNum] = useState(0)
   function getRandRace() {
     const randNum = Math.floor(Math.random() * races.length);
-    console.log(races[randNum]);
+    setrandNum(randNum);
   }
 
   return (
     <div style={{ backgroundColor: "yellow" }}>
       <h2>Race</h2>
       <button onClick={getRandRace}>Randomize</button>
+      { randNum > 0 ? <div>{(races[randNum])}</div> : null }
     </div>
   );
 }
